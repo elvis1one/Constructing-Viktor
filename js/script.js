724acf3829bedce4f73d08185372e1d5ccbe987a
elvis1one <94808155+elvis1one@users.noreply.github.com>
2023-06-22 15:57:38 +0300

    Roma help

--- js/script.js
+++ js/script.js
@@ -1,45 +1,5 @@
 
-// //--------------------------sliderForImages
 
-// const images = document.querySelectorAll('.slider__line img');
-// const sliderLine = document.querySelector('.slider__line');
-// let count = 0;
-// let width;
-
-// function init() {
-// 	console.log('resize');
-// 	width = document.querySelector('.slider__container').offsetWidth;
-// 	sliderLine.style.width = width * images.length + 'px';
-// 	images.forEach(item => {
-// 		item.style.width = width + 'px';
-// 		item.style.height = 'auto';
-// 	});
-// 	rollSlider();
-// }
-
-// window.addEventListener('resize', init);
-// init();
-
-
-// document.querySelector('.slider-next').addEventListener('click', function () {
-// 	count++;
-// 	if (count >= images.length) {
-// 		count = 0;
-// 	}
-// 	rollSlider();
-// })
-
-// document.querySelector('.slider-prev').addEventListener('click', function () {
-// 	count--;
-// 	if (count < 0) {
-// 		count = images.length - 1;
-// 	}
-// 	rollSlider();
-// })
-
-// function rollSlider() {
-// 	sliderLine.style.transform = 'translate(-' + count * width + 'px)';
-// }
 
 
 
@@ -83,4 +43,20 @@ if (animItems.length > 0) {
 	setTimeout(() => {
 		animOnScroll();
 	}, 500);
+}
+
+
+// ============add <br> to text on small windowWidth==============
+
+window.addEventListener('resize', insertLineBreak);
+
+function insertLineBreak() {
+	const textElement = document.getElementById('text');
+	const windowWidth = window.innerWidth;
+
+	if (windowWidth <= 768) {
+		textElement.innerHTML = 'Ручна та машинна штукатурка<br> в Києві та передмісті';
+	} else {
+		textElement.innerHTML = 'Ручна та машинна штукатурка в Києві та передмісті';
+	}
 }
