825cde6e072eeecb245effb16abbfc0df601a0f3
elvis1one <94808155+elvis1one@users.noreply.github.com>
2023-07-11 19:45:59 +0300

    add php mailer

--- js/script.js
+++ js/script.js
@@ -48,60 +48,23 @@ if (animItems.length > 0) {
 
 
 //===============form to e-mail=======
+$(document).ready(function () {
 
-document.addEventListener('DOMContentLoaded', function () {
-	const form = document.getElementById('form');
-	form.addEventListener('submit', formSend);
+	//E-mail Ajax Send
+	$("form").submit(function () { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function () {
+			alert("Дякуємо! Ваші дані успішно надіслано");
+			setTimeout(function () {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
-	async function formSend(e) {
-		e.preventDefault();
-
-		let error = formValidate(form);
-
-		let formData = new FormData(form);
-
-		if (error === 0) {
-			form.classList.add('sending');
-			let response = await fetch('sendmail.php', {
-				method: 'POST',
-				body: formData
-			});
-			if (response.ok) {
-				let result = await response.json();
-				alert(result.message);
-				formPreview.innerHTML = '';
-				form.reset();
-				form.classList.remove('sending');
-			} else {
-				alert('Помилка');
-				form.classList.remove('sending');
-			}
-		} else {
-			alert("Заповніть, будь ласка, обов'язкові поля");
-		}
-	}
-
-	function formValidate(form) {
-		let error = 0;
-		let formReq = document.querySelectorAll('.req');
-
-		for (let index = 0; index < formReq.length; index++) {
-			const input = formReq[index];
-			formRemoveError(input);
-
-			if (input.value === '') {
-				formAddError(input);
-				error++;
-			}
-		}
-	}
-
-	function formAddError(input) {
-		input.parentElement.classList.add('error');
-		input.classList.add('error')
-	}
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('error');
-		input.classList.remove('error')
-	}
 });
